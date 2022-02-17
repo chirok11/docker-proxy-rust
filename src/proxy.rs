af1cb3cc1420e4f02a6e4cd04e412da7478f20dc
XoXo <chirok11@protonmail.ch>
2022-02-18 00:37:12 +0400

    workin

--- src/proxy.rs
+++ src/proxy.rs
@@ -7,7 +7,8 @@ pub(crate) async fn tcp_listen_proxy(listen_ip: String, listen_port: u16, target
     while let Ok((tcp_stream, _socket_addr)) = listener.accept().await {
         let target_ip = target_ip.clone();
         tokio::spawn(async move {
-            process_socket(tcp_stream, target_ip, target_port)
+            debug!("new incoming connection");
+            process_socket(tcp_stream, target_ip, target_port).await.unwrap()
         });
     }
 
@@ -15,8 +16,11 @@ pub(crate) async fn tcp_listen_proxy(listen_ip: String, listen_port: u16, target
 }
 
 async fn process_socket(mut socket: TcpStream, target_ip: String, target_port: u16) -> Result<(u64, u64), std::io::Error> {
+    debug!("connecting");
     let mut endpoint = TcpStream::connect(format!("{}:{}", target_ip, target_port)).await?;
+    debug!("connected to endpoint {}:{}", target_ip, target_port);
     let (r, w) = tokio::io::copy_bidirectional(&mut socket, &mut endpoint).await?;
+    debug!("copied {}:{}", r, w);
 
     Ok((r, w))
 }
